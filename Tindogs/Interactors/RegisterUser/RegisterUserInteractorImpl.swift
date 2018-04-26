5dd33e38479c57f870da2ac538a433e8042ff625
Fabio Gomez <gomez.fabio@gmail.com>
2018-04-27 00:22:28 +0200

    include token at register, now it is necessary due to location impl

--- Tindogs/Interactors/RegisterUser/RegisterUserInteractorImpl.swift
+++ Tindogs/Interactors/RegisterUser/RegisterUserInteractorImpl.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class RegisterUserInteractorImpl : RegisterUserInteractor {
        
-    func execute(user: User, onSuccess: @escaping (User) -> Void, onError: errorClosure) {
+    func execute(user: User, onSuccess: @escaping (User, _ token: String) -> Void, onError: errorClosure) {
         
         let request: URLRequest = RegisterUserRequest(user: user)
         
@@ -10,8 +10,8 @@ class RegisterUserInteractorImpl : RegisterUserInteractor {
         
             OperationQueue.main.addOperation{
                 if error == nil {
-                    let user: User? = registerUserParseData(data: data!)
-                    onSuccess(user!)
+                    let (user: User, token: String) = registerUserParseData(data: data!)
+                    onSuccess(User, String)
                 } else {
                     if let onError = onError{
                         onError(error!)
